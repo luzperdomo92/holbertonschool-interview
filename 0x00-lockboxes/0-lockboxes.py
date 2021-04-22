0a5da253c8a046e8d33a360e47465f5aed028c3e
Luz Perdomo <lilyperdomo92@gmail.com>
2021-04-22 10:03:05 -0500

    Fixed indentation

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -4,18 +4,18 @@
 
 def canUnlockAll(boxes):
 
-  if (type(boxes) is not list):
-    return False
+    if (type(boxes) is not list):
+        return False
 
-  if (len(boxes) == 0):
-    return False
+    if (len(boxes) == 0):
+        return False
 
-  key = [0]
-  for array in key:
-    for elem in boxes[array]:
-      if elem not in key and elem != array and elem < len(boxes) and elem != 0:
-        key.append(elem)
-  if len(key) == len(boxes):
-    return True
-  else:
-    return False
+    key = [0]
+    for array in key:
+        for elem in boxes[array]:
+            if elem not in key and elem != array and elem < len(boxes) and elem != 0:
+                key.append(elem)
+    if len(key) == len(boxes):
+        return True
+    else:
+        return False
