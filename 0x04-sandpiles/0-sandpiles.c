e1bd1a010cad3c530c310dd963f0a1f2975c1438
Luz Perdomo <lilyperdomo92@gmail.com>
2021-05-19 15:20:36 -0500

    fixed betty

--- 0x04-sandpiles/0-sandpiles.c
+++ 0x04-sandpiles/0-sandpiles.c
@@ -8,40 +8,40 @@
  */
 void topple(int grid1[3][3])
 {
-  int grains[3][3];
-  int i, j;
+	int grains[3][3];
+	int i, j;
 
-  for (i = 0; i < 3; i++)
-  {
-    for (j = 0; j < 3; j++)
-    {
-      grains[i][j] = 0;
-    }
-  }
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 3; j++)
+		{
+			grains[i][j] = 0;
+		}
+	}
 
-  for (i = 0; i < 3; i++)
-  {
-    for (j = 0; j < 3; j++)
-    {
-      if (grid1[i][j] > 3)
-      {
-        /* left */
-        if ((i - 1 >= 0) && (i - 1 < 3))
-          grains[i - 1][j]++;
-        /* right */
-        if ((i + 1 >= 0) && (i + 1 < 3))
-          grains[i + 1][j]++;
-        /* up */
-        if ((j - 1 >= 0) && (j - 1 < 3))
-          grains[i][j - 1]++;
-        /* down */
-        if ((j + 1 >= 0) && (j + 1 < 3))
-          grains[i][j + 1]++;
-        grid1[i][j] -= 4;
-      }
-    }
-  }
-  add_grids(grid1, grains);
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 3; j++)
+		{
+			if (grid1[i][j] > 3)
+			{
+				/* left */
+				if ((i - 1 >= 0) && (i - 1 < 3))
+					grains[i - 1][j]++;
+				/* right */
+				if ((i + 1 >= 0) && (i + 1 < 3))
+					grains[i + 1][j]++;
+				/* up */
+				if ((j - 1 >= 0) && (j - 1 < 3))
+					grains[i][j - 1]++;
+				/* down */
+				if ((j + 1 >= 0) && (j + 1 < 3))
+					grains[i][j + 1]++;
+				grid1[i][j] -= 4;
+			}
+		}
+	}
+	add_grids(grid1, grains);
 }
 
 /**
@@ -50,19 +50,19 @@ void topple(int grid1[3][3])
  */
 void alt_print_grid(int grid1[3][3])
 {
-  int i, j;
+	int i, j;
 
-  printf("=\n");
-  for (i = 0; i < 3; i++)
-  {
-    for (j = 0; j < 3; j++)
-    {
-      if (j)
-        printf(" ");
-      printf("%d", grid1[i][j]);
-    }
-    printf("\n");
-  }
+	printf("=\n");
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 3; j++)
+		{
+			if (j)
+				printf(" ");
+			printf("%d", grid1[i][j]);
+		}
+		printf("\n");
+	}
 }
 
 /**
@@ -74,17 +74,17 @@ void alt_print_grid(int grid1[3][3])
  */
 int is_stable(int grid1[3][3])
 {
-  int i, j;
+	int i, j;
 
-  for (i = 0; i < 3; i++)
-  {
-    for (j = 0; j < 3; j++)
-    {
-      if (grid1[i][j] > 3)
-        return (FALSE_);
-    }
-  }
-  return (TRUE_);
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 3; j++)
+		{
+			if (grid1[i][j] > 3)
+				return (FALSE_);
+		}
+	}
+	return (TRUE_);
 }
 
 /**
@@ -94,13 +94,13 @@ int is_stable(int grid1[3][3])
  */
 void add_grids(int grid1[3][3], int grid2[3][3])
 {
-  int i, j;
+	int i, j;
 
-  for (i = 0; i < 3; i++)
-  {
-    for (j = 0; j < 3; j++)
-      grid1[i][j] = grid1[i][j] + grid2[i][j];
-  }
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 3; j++)
+			grid1[i][j] = grid1[i][j] + grid2[i][j];
+	}
 }
 
 /**
@@ -110,13 +110,13 @@ void add_grids(int grid1[3][3], int grid2[3][3])
  */
 void sandpiles_sum(int grid1[3][3], int grid2[3][3])
 {
-  add_grids(grid1, grid2);
-  if (!is_stable(grid1))
-    alt_print_grid(grid1);
-  while (!is_stable(grid1))
-  {
-    topple(grid1);
-    if (!is_stable(grid1))
-      alt_print_grid(grid1);
-  }
+	add_grids(grid1, grid2);
+	if (!is_stable(grid1))
+		alt_print_grid(grid1);
+	while (!is_stable(grid1))
+	{
+		topple(grid1);
+		if (!is_stable(grid1))
+			alt_print_grid(grid1);
+	}
 }
