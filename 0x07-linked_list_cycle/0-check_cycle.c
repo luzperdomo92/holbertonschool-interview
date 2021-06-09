0b906c21523ebe31553bc02f60ed2ad1a09c5110
Luz Perdomo <lilyperdomo92@gmail.com>
2021-06-09 09:53:30 -0500

    fixed betty

--- 0x07-linked_list_cycle/0-check_cycle.c
+++ 0x07-linked_list_cycle/0-check_cycle.c
@@ -6,17 +6,17 @@
 */
 int check_cycle(listint_t *list)
 {
-    listint_t *turtle = list, *hare = list;
+	listint_t *turtle = list, *hare = list;
 
-    if (list == NULL)
-        return (0);
+	if (list == NULL)
+		return (0);
 
-    while (hare != NULL && hare->next != NULL && hare->next->next != NULL)
-    {
-        turtle = turtle->next;
-        hare = hare->next->next;
-        if (turtle == hare)
-            return (1);
-        }
-    return (0);
+	while (hare != NULL && hare->next != NULL && hare->next->next != NULL)
+	{
+		turtle = turtle->next;
+		hare = hare->next->next;
+		if (turtle == hare)
+			return (1);
+		}
+	return (0);
 }
