c80af977d559255a565de3d9dc51445fdcbfed9e
Luz Perdomo <lilyperdomo92@gmail.com>
2021-08-05 09:41:05 -0500

    added directory with all files

--- 0x0E-linear_skip/0-linear_skip.c
+++ 0x0E-linear_skip/0-linear_skip.c
@@ -10,42 +10,45 @@
  **/
 skiplist_t *linear_skip(skiplist_t *list, int value)
 {
-  skiplist_t *skip;
+	skiplist_t *skip;
 
-  if (!list)
-	return (NULL);
+	if (!list)
+		return (NULL);
 
-  skip = list;
-  while (skip->express)
-  {
-	printf("Value checked at index [%lu] = [%d]\n",
-	  skip->express->index, skip->express->n);
-	if (skip->express->n >= value)
+	skip = list;
+	while (skip->express)
 	{
-	  printf("Value found between indexes [%lu] and [%lu]\n",
-		skip->index, skip->express->index);
-	  break;
+		printf("Value checked at index [%lu] = [%d]\n",
+			skip->express->index, skip->express->n);
+		if (skip->express->n >= value)
+		{
+	  		printf("Value found between indexes [%lu] and [%lu]\n",
+				skip->index, skip->express->index);
+	  	break;
+		}
+		skip = skip->express;
 	}
-	skip = skip->express;
-  }
-  if (!skip->express)
-  {
+
+	if (!skip->express)
+	{
+		list = skip;
+		while (list->next)
+			list = list->next;
+			printf("Value found between indexes [%lu] and [%lu]\n",
+		  		skip->index, list->index);
+	}
 	list = skip;
-	while (list->next)
-	  list = list->next;
-	printf("Value found between indexes [%lu] and [%lu]\n",
-	  skip->index, list->index);
-  }
-  list = skip;
-  while (list != skip->express)
-  {
-	printf("Value checked at index [%lu] = [%d]\n",
-	  list->index, list->n);
-	if (list->n == value)
-	  break;
-	list = list->next;
-  }
-  if (list != skip->express)
-	return (list);
-  return (NULL);
+	while (list != skip->express)
+	{
+		printf("Value checked at index [%lu] = [%d]\n",
+		  list->index, list->n);
+		if (list->n == value)
+			break;
+		list = list->next;
+	}
+
+	if (list != skip->express)
+		return (list);
+
+	return (NULL);
 }
