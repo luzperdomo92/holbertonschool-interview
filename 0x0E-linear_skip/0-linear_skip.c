014c2f22ca8aa3649372c083a289c39d3263a70e
Luz Perdomo <lilyperdomo92@gmail.com>
2021-08-05 09:47:00 -0500

    fixed betty styles

--- 0x0E-linear_skip/0-linear_skip.c
+++ 0x0E-linear_skip/0-linear_skip.c
@@ -11,10 +11,8 @@
 skiplist_t *linear_skip(skiplist_t *list, int value)
 {
 	skiplist_t *skip;
-
 	if (!list)
 		return (NULL);
-
 	skip = list;
 	while (skip->express)
 	{
@@ -22,20 +20,19 @@ skiplist_t *linear_skip(skiplist_t *list, int value)
 			skip->express->index, skip->express->n);
 		if (skip->express->n >= value)
 		{
-	  		printf("Value found between indexes [%lu] and [%lu]\n",
+			printf("Value found between indexes [%lu] and [%lu]\n",
 				skip->index, skip->express->index);
-	  	break;
+			break;
 		}
 		skip = skip->express;
 	}
-
 	if (!skip->express)
 	{
 		list = skip;
 		while (list->next)
 			list = list->next;
 			printf("Value found between indexes [%lu] and [%lu]\n",
-		  		skip->index, list->index);
+				skip->index, list->index);
 	}
 	list = skip;
 	while (list != skip->express)
@@ -46,9 +43,7 @@ skiplist_t *linear_skip(skiplist_t *list, int value)
 			break;
 		list = list->next;
 	}
-
 	if (list != skip->express)
 		return (list);
-
 	return (NULL);
 }
